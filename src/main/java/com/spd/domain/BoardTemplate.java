f083dcf2fdedf79af13a0858b3b3ac3b01f75ffd
Oleg Cherepnin <cherepnin11@gmail.com>
2021-12-07 16:41:50 +0200

    create class BoardTemplate

---
+++ src/main/java/com/spd/domain/BoardTemplate.java
@@ -0,0 +1,20 @@
+package com.spd.domain;
+
+import java.util.List;
+
+public class BoardTemplate extends Board{
+    private String title;
+
+    public BoardTemplate(String name, String description, List<CardList> cardList, String title) {
+        super(name, description, cardList);
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
