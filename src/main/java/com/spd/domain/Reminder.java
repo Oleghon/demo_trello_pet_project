075ea0316f4a2ea0b6aa8c327aaf59fbf4851adc
Oleg Cherepnin <cherepnin11@gmail.com>
2021-12-07 10:13:45 +0200

    create class Reminder

---
+++ src/main/java/com/spd/domain/Reminder.java
@@ -0,0 +1,51 @@
+package com.spd.domain;
+
+import java.time.LocalDateTime;
+
+public class Reminder {
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private LocalDateTime remindOn;
+    private boolean isAlive;
+
+    public Reminder() {}
+
+    public Reminder(LocalDateTime start, LocalDateTime end, LocalDateTime remindOn, boolean isAlive) {
+        this.start = start;
+        this.end = end;
+        this.remindOn = remindOn;
+        this.isAlive = isAlive;
+    }
+
+    public LocalDateTime getStart() {
+        return start;
+    }
+
+    public void setStart(LocalDateTime start) {
+        this.start = start;
+    }
+
+    public LocalDateTime getEnd() {
+        return end;
+    }
+
+    public void setEnd(LocalDateTime end) {
+        this.end = end;
+    }
+
+    public LocalDateTime getRemindOn() {
+        return remindOn;
+    }
+
+    public void setRemindOn(LocalDateTime remindOn) {
+        this.remindOn = remindOn;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
+}
