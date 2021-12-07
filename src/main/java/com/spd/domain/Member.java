2cf999687454ad857ea9ac9accaf25f27e2f2d25
Oleg Cherepnin <cherepnin11@gmail.com>
2021-12-07 10:07:46 +0200

    create class Member

---
+++ src/main/java/com/spd/domain/Member.java
@@ -0,0 +1,27 @@
+package com.spd.domain;
+
+public class Member {
+    private User user;
+    private Role role;
+
+    public Member(User user, Role role) {
+        this.user = user;
+        this.role = role;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
