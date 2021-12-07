9bd2ad619b35092f95051cd07c40463cf76e2fbd
Oleg Cherepnin <cherepnin11@gmail.com>
2021-12-07 10:23:39 +0200

    create class CheckableItem

---
+++ src/main/java/com/spd/domain/CheckableItem.java
@@ -0,0 +1,29 @@
+package com.spd.domain;
+
+public class CheckableItem {
+    private String name;
+    private boolean check;
+
+    public CheckableItem() {}
+
+    public CheckableItem(String name, boolean check) {
+        this.name = name;
+        this.check = check;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isCheck() {
+        return check;
+    }
+
+    public void setCheck(boolean check) {
+        this.check = check;
+    }
+}
