565eb09fec10bdbf78c849f9eaae2f73c6b83081
Oleg Cherepnin <cherepnin11@gmail.com>
2021-12-07 11:24:09 +0200

    create class CardList

---
+++ src/main/java/com/spd/domain/CardList.java
@@ -0,0 +1,39 @@
+package com.spd.domain;
+
+import java.util.List;
+
+public class CardList {
+    private String name;
+    private List<Card> cards;
+    private boolean isArchived;
+
+    public CardList(String name, List<Card> cards, boolean isArchived) {
+        this.name = name;
+        this.cards = cards;
+        this.isArchived = isArchived;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    public void setCards(List<Card> cards) {
+        this.cards = cards;
+    }
+
+    public boolean isArchived() {
+        return isArchived;
+    }
+
+    public void setArchived(boolean archived) {
+        isArchived = archived;
+    }
+}
