d35225a2cca1c608500f076e103193b36a99c57b
Oleg Cherepnin <cherepnin11@gmail.com>
2021-12-07 11:33:04 +0200

    create class CardTemplate

---
+++ src/main/java/com/spd/domain/CardTemplate.java
@@ -0,0 +1,26 @@
+package com.spd.domain;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class CardTemplate extends Card{
+    private String title;
+
+    public CardTemplate(String name, String description, boolean isArchived,
+                        LocalDateTime creationDate, List<Member> assignedMembers, String title) {
+        super(name, description, isArchived, creationDate, assignedMembers);
+        this.title = title;
+    }
+
+    public CardTemplate(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
