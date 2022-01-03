08699c250c83a84577ca6f3260558617ba842512
Oleg Cherepnin <cherepnin11@gmail.com>
2022-01-03 20:39:37 +0200

    update WorkSpaceRepository

--- src/main/java/com/spd/trello/repository/impl/WorkSpaceRepositoryImpl.java
+++ src/main/java/com/spd/trello/repository/impl/WorkSpaceRepositoryImpl.java
@@ -5,6 +5,7 @@ import com.spd.trello.domain.WorkSpace;
 import com.spd.trello.domain.WorkSpaceVisibility;
 import com.spd.trello.repository.Repository;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -22,8 +23,8 @@ public class WorkSpaceRepositoryImpl implements Repository<WorkSpace> {
 
     @Override
     public WorkSpace create(WorkSpace obj) {
-        try (PreparedStatement statement = config.getConnection()
-                .prepareStatement("INSERT INTO workspaces(id, created_by, created_date, name, description, visibility) " +
+        try (Connection connection = config.getConnection(); PreparedStatement statement =
+                connection.prepareStatement("INSERT INTO workspaces(id, created_by, created_date, name, description, visibility) " +
                         "VALUES(?,?,?,?,?,?)")) {
             statement.setObject(1, obj.getId());
             statement.setString(2, obj.getCreatedBy());
@@ -32,15 +33,15 @@ public class WorkSpaceRepositoryImpl implements Repository<WorkSpace> {
             statement.setString(5, obj.getDescription());
             statement.setString(6, String.valueOf(obj.getVisibility()));
             statement.executeUpdate();
-            addMemberRelations(obj);
+            addMemberRelations(obj, connection);
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return obj;
     }
 
-    private void addMemberRelations(WorkSpace workSpace) throws SQLException {
-        try (PreparedStatement statement = config.getConnection()
+    private void addMemberRelations(WorkSpace workSpace, Connection connection) throws SQLException {
+        try (PreparedStatement statement = connection
                 .prepareStatement("INSERT INTO space_member(space_id, member_id) VALUES (?,?)")) {
 
             for (Member member : workSpace.getWorkspaceMembers()) {
@@ -53,8 +54,8 @@ public class WorkSpaceRepositoryImpl implements Repository<WorkSpace> {
 
     @Override
     public WorkSpace update(UUID index, WorkSpace obj) {
-        try (PreparedStatement statement = config.getConnection()
-                .prepareStatement("UPDATE workspaces SET updated_by=?, updated_date=?, name=?, description=?, visibility=? where id=?")) {
+        try (Connection connection = config.getConnection(); PreparedStatement statement =
+                connection.prepareStatement("UPDATE workspaces SET updated_by=?, updated_date=?, name=?, description=?, visibility=? where id=?")) {
             statement.setString(1, obj.getUpdatedBy());
             statement.setObject(2, obj.getUpdatedDate());
             statement.setString(3, obj.getName());
@@ -62,9 +63,9 @@ public class WorkSpaceRepositoryImpl implements Repository<WorkSpace> {
             statement.setString(5, String.valueOf(obj.getVisibility()));
             statement.setObject(6, index);
             statement.executeUpdate();
-//            obj.getWorkspaceMembers().removeAll(getUsersForWorkSpace(index));
-//            if (!obj.getWorkspaceMembers().isEmpty())
-//                addMemberRelations(obj);
+            obj.getWorkspaceMembers().removeAll(getMembersForWorkSpace(index));
+            if (!obj.getWorkspaceMembers().isEmpty())
+                addMemberRelations(obj, connection);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -81,6 +82,7 @@ public class WorkSpaceRepositoryImpl implements Repository<WorkSpace> {
                 ResultSet resultSet = statement.getResultSet();
                 resultSet.next();
                 foundSpace = buildWorkSpace(resultSet);
+                foundSpace.setWorkspaceMembers(getMembersForWorkSpace(index));
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -96,12 +98,11 @@ public class WorkSpaceRepositoryImpl implements Repository<WorkSpace> {
         workSpace.setVisibility(WorkSpaceVisibility.valueOf(resultSet.getString("visibility")));
         workSpace.setName(resultSet.getString("name"));
         workSpace.setDescription(resultSet.getString("description"));
-        workSpace.setWorkspaceMembers(getUsersForWorkSpace(workSpace.getId()));
-//        workSpace.setBoardList();
+//        workSpace.setBoardList(); // TODO
         return workSpace;
     }
 
-    private List<Member> getUsersForWorkSpace(UUID spaceId) throws SQLException {
+    public List<Member> getMembersForWorkSpace(UUID spaceId) throws SQLException {
         List<Member> members = new ArrayList<>();
         try (PreparedStatement statement = config.getConnection()
                 .prepareStatement("select m.id as id, m.created_by as created_by, m.updated_by as updated_by, m.created_date as created_date, m.role as role, m.user_id as user_id " +
@@ -119,9 +120,9 @@ public class WorkSpaceRepositoryImpl implements Repository<WorkSpace> {
 
     @Override
     public void delete(UUID index) {
-        try (PreparedStatement statement = config.getConnection()
+        try (Connection connection = config.getConnection(); PreparedStatement statement = connection
                 .prepareStatement("DELETE FROM workspaces WHERE id = ?")) {
-            deleteRelations(index);
+            deleteRelations(index, connection);
             statement.setObject(1, index);
             statement.executeUpdate();
         } catch (SQLException e) {
@@ -129,8 +130,8 @@ public class WorkSpaceRepositoryImpl implements Repository<WorkSpace> {
         }
     }
 
-    private void deleteRelations(UUID index) throws SQLException {
-        try (PreparedStatement statement = config.getConnection()
+    private void deleteRelations(UUID index, Connection connection) throws SQLException {
+        try (PreparedStatement statement = connection
                 .prepareStatement("DELETE FROM space_member WHERE space_id = ?")) {
             statement.setObject(1, index);
             statement.executeUpdate();
