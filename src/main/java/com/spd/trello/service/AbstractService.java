afe7908d125a52ed9a5c6a1fddde3a0bc85dd3bf
Oleg Cherepnin <cherepnin11@gmail.com>
2022-01-05 19:57:12 +0200

    refactor AbstractService

--- src/main/java/com/spd/trello/service/AbstractService.java
+++ src/main/java/com/spd/trello/service/AbstractService.java
@@ -2,28 +2,18 @@ package com.spd.trello.service;
 
 import com.spd.trello.domain.Resource;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public abstract class AbstractService<T extends Resource> {
-    protected List<T> items = new ArrayList();
 
-    public abstract T create(boolean addToList);
+    public abstract T create(T obj);
 
-    public abstract T update(int index, T obj);
+    public abstract T update(UUID id, T obj);
 
-//    public abstract T readByIndex(int index);
+    public abstract T readById(UUID id);
 
-//    public abstract T delete(int index);
+    public abstract boolean delete(UUID id);
 
-    public void show() {
-        if (items.size() != 0)
-            System.out.println(items.get(items.size() - 1));
-        else
-            System.out.println("You don`t create any item");
-    }
-
-    public void showItems() {
-        System.out.println(items);
-    }
+    public abstract List<T> readAll();
 }
