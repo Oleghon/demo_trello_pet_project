0b2b7ab903aa78c8b58472895f3fa4d7366c7a43
Oleg Cherepnin <cherepnin11@gmail.com>
2022-01-12 16:32:19 +0200

    create helpers for Attachment and CheckableItems

---
+++ src/main/java/com/spd/trello/repository/impl/helper/AttachmentHelper.java
@@ -0,0 +1,118 @@
+package com.spd.trello.repository.impl.helper;
+
+import com.spd.trello.config.JdbcConfig;
+import com.spd.trello.domain.Attachment;
+import com.spd.trello.domain.Card;
+import com.spd.trello.domain.Comment;
+import com.spd.trello.repository.Repository;
+
+import java.io.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
+import java.util.UUID;
+
+public class AttachmentHelper {
+
+    public Attachment create(Attachment attachment, Card card) {
+        try (Connection connection = JdbcConfig.getConnection();
+             PreparedStatement statement = connection
+                     .prepareStatement("insert into attachments(id, link, name, file, card_id) values (?,?,?,?,?)")) {
+            statement.setObject(1, attachment.getId());
+            statement.setString(2, attachment.getLink());
+            statement.setString(3, attachment.getName());
+            statement.setBytes(4, fileToByte(attachment.getFile()));
+            statement.setObject(5, card.getId());
+            statement.executeUpdate();
+        } catch (SQLException | FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return findById(attachment.getId());
+    }
+
+    public Attachment create(Attachment attachment, Comment comment) {
+        try (Connection connection = JdbcConfig.getConnection();
+             PreparedStatement statement = connection
+                     .prepareStatement("insert into attachments(id, link, name, file, comment_id) values (?,?,?,?,?)")) {
+            statement.setObject(1, attachment.getId());
+            statement.setString(2, attachment.getLink());
+            statement.setString(3, attachment.getName());
+            statement.setObject(4, fileToByte(attachment.getFile()));
+            statement.setObject(5, comment.getId());
+            statement.executeUpdate();
+        } catch (IOException | SQLException e) {
+            e.printStackTrace();
+        }
+        return findById(attachment.getId());
+    }
+
+    public Attachment findById(UUID id) {
+        Attachment foundAttachment = null;
+        try (Connection connection = JdbcConfig.getConnection();
+             PreparedStatement statement = connection
+                     .prepareStatement("select * from attachments where id = ?")) {
+            statement.setObject(1, id);
+            if (statement.execute()) {
+                ResultSet resultSet = statement.executeQuery();
+                resultSet.next();
+                foundAttachment = buildAttachment(resultSet);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return foundAttachment;
+    }
+
+    private Attachment buildAttachment(ResultSet resultSet) throws SQLException {
+        Attachment attachment = new Attachment();
+        attachment.setId(UUID.fromString(resultSet.getString("id")));
+        attachment.setName(resultSet.getString("name"));
+        attachment.setLink(resultSet.getString("link"));
+        attachment.setFile(writeBytesToFile(new File("db_files/" + attachment.getName() + ".jpg"),
+                resultSet.getBytes("file")));
+
+        UUID cardId = Optional.ofNullable(resultSet.getString("card_id"))
+                .map(UUID::fromString).orElse(null);
+        if (cardId != null) {
+            Card card = new Card();
+            card.setId(cardId);
+            attachment.setCard(card);
+        } else {
+            Comment comment = new Comment();
+            comment.setId(Optional.ofNullable(resultSet.getString("comment_id"))
+                    .map(UUID::fromString).orElse(null));
+            attachment.setComment(comment);
+        }
+        return attachment;
+    }
+
+    public boolean delete(UUID id) throws SQLException {
+        return new Repository.Helper().delete(id, "delete from attachments where id = ?");
+    }
+
+    public byte[] fileToByte(File file) throws FileNotFoundException {
+        FileInputStream fileInputStream = new FileInputStream(file);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        byte[] bytes1 = new byte[1024];
+        try {
+            for (int readNum; (readNum = fileInputStream.read(bytes1)) != -1; ) {
+                byteArrayOutputStream.write(bytes1, 0, readNum);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+        return bytes;
+    }
+
+    private File writeBytesToFile(File file, byte[] bytes) {
+        try (FileOutputStream fos = new FileOutputStream(file)) {
+            fos.write(bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return file;
+    }
+}
---
+++ src/main/java/com/spd/trello/repository/impl/helper/ItemsHelper.java
@@ -0,0 +1,47 @@
+package com.spd.trello.repository.impl.helper;
+
+import com.spd.trello.config.JdbcConfig;
+import com.spd.trello.domain.CheckList;
+import com.spd.trello.domain.CheckableItem;
+import com.spd.trello.repository.Repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.UUID;
+
+public class ItemsHelper {
+
+    public CheckableItem create(CheckableItem obj) {
+        try (Connection connection = JdbcConfig.getConnection();
+             PreparedStatement statement = connection
+                     .prepareStatement("insert into items(id, name, checked, checklist_id)" +
+                             "values (?,?,?,?)")) {
+            statement.setObject(1, obj.getId());
+            statement.setString(2, obj.getName());
+            statement.setBoolean(3, obj.getCheck());
+            statement.setObject(4, obj.getCheckList().getId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return obj;
+    }
+
+    public static CheckableItem buildItem(ResultSet resultSet) throws SQLException {
+        CheckableItem item = new CheckableItem();
+        CheckList checkList = new CheckList();
+        item.setId(UUID.fromString(resultSet.getString("id")));
+        item.setName(resultSet.getString("name"));
+        item.setCheck(resultSet.getBoolean("checked"));
+        checkList.setId(UUID.fromString(resultSet.getString("checklist_id")));
+        item.setCheckList(checkList);
+        return item;
+    }
+
+    public boolean delete(UUID id) {
+        return new Repository.Helper().delete(id, "delete from items where id =?");
+    }
+
+}
