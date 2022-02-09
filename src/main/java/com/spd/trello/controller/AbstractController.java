353be37011ad775db31b25a1f522e7eae82a2cb0
Oleg Cherepnin <cherepnin11@gmail.com>
2022-02-09 17:16:46 +0200

    update AbstractController

--- src/main/java/com/spd/trello/controller/AbstractController.java
+++ src/main/java/com/spd/trello/controller/AbstractController.java
@@ -1,7 +1,7 @@
 package com.spd.trello.controller;
 
 import com.spd.trello.domain.Resource;
-import com.spd.trello.service.AbstractService;
+import com.spd.trello.service.CommonService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
-public class AbstractController<E extends Resource, S extends AbstractService<E>>
+public class AbstractController<E extends Resource, S extends CommonService<E>>
         implements IController<E> {
 
     S service;
@@ -21,6 +21,7 @@ public class AbstractController<E extends Resource, S extends AbstractService<E>
     @PostMapping
     @Override
     public ResponseEntity<E> create(@RequestBody E resource) {
+        resource.setCreatedBy("chekchek@gmail.com");
         E result = service.create(resource);
         return new ResponseEntity<>(result, HttpStatus.CREATED);
     }
@@ -28,8 +29,8 @@ public class AbstractController<E extends Resource, S extends AbstractService<E>
     @PutMapping("/{id}")
     @Override
     public ResponseEntity<E> update(@PathVariable UUID id, @RequestBody E resource) {
-        E result = service.update(id, resource);
-        //todo
+        resource.setUpdatedBy("chekchek@gmail.com");
+        E result = service.update(resource);
         return new ResponseEntity<>(result, HttpStatus.RESET_CONTENT);
     }
 
@@ -43,7 +44,8 @@ public class AbstractController<E extends Resource, S extends AbstractService<E>
     @DeleteMapping("/{id}")
     @Override
     public HttpStatus delete(@PathVariable UUID id) {
-        return service.delete(id) ? HttpStatus.OK : HttpStatus.NOT_FOUND;
+        service.delete(id);
+        return HttpStatus.OK;
     }
 
     @GetMapping
