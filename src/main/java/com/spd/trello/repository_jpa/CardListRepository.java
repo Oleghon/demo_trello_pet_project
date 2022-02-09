1377f032eaad71ccd4cf165d8cda8e344f3389ea
Oleg Cherepnin <cherepnin11@gmail.com>
2022-02-09 17:19:52 +0200

    clean-up repository layer

--- src/main/java/com/spd/trello/repository_jpa/CardListRepository.java
+++ src/main/java/com/spd/trello/repository_jpa/CardListRepository.java
@@ -3,10 +3,6 @@ package com.spd.trello.repository_jpa;
 import com.spd.trello.domain.resources.CardList;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.UUID;
-
 @Repository
 public interface CardListRepository extends CommonRepository<CardList> {
-    List<CardList> findAllByBoardId(UUID id);
 }
